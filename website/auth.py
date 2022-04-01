0175aa2e4651ddbba3e965f50627a8a1867f4529
kmitova <96522051+kmitova@users.noreply.github.com>
2022-04-01 17:21:01 +0300

    http and message flashing

--- website/auth.py
+++ website/auth.py
@@ -1,12 +1,12 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 # set a blueprint for the application
 auth = Blueprint("auth", __name__)
 
 
-@auth.route("/login")
+@auth.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("login.html", boolean=False)
+    return render_template("login.html", boolean=True)
 
 
 @auth.route("/logout")
@@ -14,6 +14,24 @@ def logout():
     return "<p>Logout</p>"
 
 
-@auth.route("/sign-up")
+@auth.route("/sign-up", methods=["GET", "POST"])
 def sign_up():
+    if request.method == "POST":
+        email = request.form.get("email")
+        firstName = request.form.get("firstName")
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
+
+        if len(email) < 4:
+            flash("Email must be longer than 3 characters.", category="error")
+        elif len(firstName) < 2:
+            flash("First name must be longer than 1 character.", category="error")
+        elif not password1 == password2:
+            flash("Passwords don't match.", category="error")
+        elif len(password1) < 7:
+            flash("Password must be at least 7 characters.", category="error")
+        else:
+            flash("Account created!", category="success")
+            # add user to database
+            
     return render_template("signup.html")
